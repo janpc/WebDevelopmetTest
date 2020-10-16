849828e40674531c98efe45ce0ed65ef34e5994a
Jan Puig <janpuigcapella@gmail.com>
2020-10-16 17:01:34 +0200

    Create homepage.htm

--- homescript.js
+++ homescript.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    sessionStorage.name = document.getElementById("myTextarea").value;
+    sessionStorage.name = document.getElementById("NickName").value;
 
     if (sessionStorage.name != "") {
       window.location = "form.html";
