4b4ec4bbe5261abb7403827dd5282e08d8419a83
Jan Puig <janpuigcapella@gmail.com>
2020-10-17 14:26:21 +0200

    Create the data structure

--- formscript.js
+++ formscript.js
@@ -5,4 +5,24 @@ window.onload= function() {
         document.getElementById("demo").innerHTML =
         "Sorry, your browser does not support web storage...";
     }
+    radioQuestion(0);
 };
+
+function radioQuestion(q) {
+    document.getElementById("question").innerHTML="<h3>"+questions[q].question+"</h3>";
+    let answers = randomArray(questions[q].answers);
+    answers.forEach(answer => {
+        document.getElementById("question").innerHTML+='<input type="radio" id="a" name="A" value="a" /><label for="a1">'+answer+'</label><br />'
+    });
+  }
+
+
+  function randomArray(array){
+    for(let i = array.length-1; i > 0; i--){
+        const j = Math.floor(Math.random() * i)
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+      }
+      return array;
+  }
---
+++ questions.js
@@ -0,0 +1,123 @@
+let questions =[
+    {
+        "question": "What does HTML stand for?",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language"]
+    },
+    {
+        "question": "What is the correct HTML for adding a background color?",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":['<body style="background-color:yellow;">', "<background>yellow</background>", '<body bg="yellow">']
+    },
+    {
+        "question": "Choose the correct HTML element or elements to define emphasized or important text:",
+        "type":"checkbox",
+        "correctAnswers": 2,
+        "answers":["<em>", "<strong>", "<italic>", "<important>"]
+    },
+    {
+        "question": "What is the correct HTML for creating a hyperlink?",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":['<a href="http://www.w3schools.com">W3Schools</a>', "<a>http://www.w3schools.com</a>", '<a name="http://www.w3schools.com">W3Schools.com</a>', '<a url="http://www.w3schools.com">W3Schools.com</a>' ]
+    },
+    {
+        "question": "Which character is used to indicate an end tag?",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":["/ ", "*", "^", "<"]
+    },
+    {
+        "question": "How can you make a numbered list?",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":["<ol>", "<list>", "<dl>", "<ul>"]
+    },
+    {
+        "question": "How can you make a bulleted list?",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":["<ul>","<ol>", "<list>", "<dl>"]
+    },
+    {
+        "question": "What is the correct HTML for making a checkbox?",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":['<input type="checkbox">', "<checkbox>", "<check>", '<input type="check">']
+    },
+
+    {
+        "question": "What is the correct HTML for making a drop-down list?",
+        "type":"drop-down",
+        "correctAnswers": 1,
+        "answers":['<select>', "<list>", '<input type="list">', '<input type="dropdown">']
+    },
+    {
+        "question": "What is the correct HTML for referring to an external style sheet?",
+        "type":"drop-down",
+        "correctAnswers": 1,
+        "answers":['<link rel="stylesheet" type="text/css" href="mystyle.css"> ', "<stylesheet>mystyle.css</stylesheet>", '<style src="mystyle.css">']
+    },
+    {
+        "question": "Which is the correct CSS syntax?",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":['body {color: black;}', "body:color=black;", '{body;color:black;}', '{body:color=black;}']
+    },
+    {
+        "question": "Which CSS property is used to change the text color of an element?",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":['color', "fgcolor", 'text-color']
+    },
+    {
+        "question": "Which CSS property controls the text size?",
+        "type":"drop-down",
+        "correctAnswers": 1,
+        "answers":['font-size', "font-style", 'text-style', 'text-size']
+    },
+    {
+        "question": "How do you display hyperlinks without an underline?",
+        "type":"drop-down",
+        "correctAnswers": 1,
+        "answers":['a {text-decoration:none;}', "a {decoration:no-underline;}", 'a {text-decoration:no-underline;}', 'a {underline:none;}']
+    },
+    {
+        "question": "What is the correct JavaScript syntax to change the content of the HTML element below?",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":['document.getElementById("demo").innerHTML = "Hello World!"', 'document.getElementByName("p").innerHTML = "Hello World!"', 'document.getElement("p").innerHTML = "Hello World!"; ', '#demo.innerHTML = "Hello World!";']
+    },
+    {
+        "question": "The external JavaScript file must contain the <script> tag:",
+        "type":"drop-down",
+        "correctAnswers": 1,
+        "answers":['False', 'True']
+    },
+    {
+        "question": "How to write an IF statement in JavaScript?",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":['if (i == 5) ', 'if i == 5 then', 'if i = 5 then', 'if i = 5']
+    },
+    {
+        "question": "How does a FOR loop start?",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":['for (i = 0; i <= 5; i++)', 'for i = 1 to 5', 'for (i <= 5; i++)', 'for (i = 0; i <= 5)']
+    },
+    {
+        "question": "JavaScript is the same as Java.",
+        "type":"drop-down",
+        "correctAnswers": 1,
+        "answers":['False ', 'True']
+    },
+    {
+        "question": "What will the following code return: Boolean(10 > 9)",
+        "type":"radio",
+        "correctAnswers": 1,
+        "answers":['True', 'False ', 'NaN']
+    },
+    ]
